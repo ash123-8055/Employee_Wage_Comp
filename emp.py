a04842493c098e59446b11185779a22a7933e59f
rk0655 <104516739+rk0655@users.noreply.github.com>
2025-01-31 14:29:17 +0530

    [Ash]Created a function to Check attendance of the employee

--- emp.py
+++ emp.py
@@ -1,1 +1,11 @@
+import random 
+
+def attendance():
+    random_checker=random.randint(0,1)
+    if random_checker==1:
+        print("The Employee is present")
+    else:
+        print("The Employee is absent")
+    
 print("Welcome to Employee Wage Computation")
+attendance()
