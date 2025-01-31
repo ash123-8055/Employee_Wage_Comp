951beb0f74615f5d2df9e65d1523be6a30fc27da
rk0655 <104516739+rk0655@users.noreply.github.com>
2025-01-31 14:44:09 +0530

    [Ash]Added switch case for Fulltime and PartTime

--- emp.py
+++ emp.py
@@ -8,13 +8,21 @@ def attendance():
         print("The Employee is absent")
 
 def wage_calculator():
-    wage_per_hour=20
-    full_day_hour=8
-    part_time_hour=4
-    daily_wage=wage_per_hour*full_day_hour
-    part_time_wage=wage_per_hour*part_time_hour
-    print("The daily wage of the employee is ", daily_wage)
-    print("The part time wage of the employee is ", part_time_wage)
+    print("Enter the employee type: 1. Full Time 2. Part Time")
+    emp_type=int(input())
+    match emp_type:
+        case 1:
+            wage_per_hour=20
+            full_day_hour=8
+            daily_wage=wage_per_hour*full_day_hour
+            print("The daily wage of the employee is ", daily_wage)
+        case 2:
+            wage_per_hour=20
+            part_time_hour=4
+            part_time_wage=wage_per_hour*part_time_hour
+            print("The part time wage of the employee is ", part_time_wage)
+        case default:
+            print("Invalid input")
 
     
 print("Welcome to Employee Wage Computation")
