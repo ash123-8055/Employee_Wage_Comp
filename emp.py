46688680cf01f6354b1c1cacfcc0db67d6b2ba3e
Ashwin <ash123.8055@gmail.com>
2025-02-16 21:45:14 +0530

    [Ashwin] Refactor: Added docstring to emp.py

--- emp.py
+++ emp.py
@@ -1,6 +1,14 @@
 import random 
 
+
 def attendance():
+    """Description: 
+        This function uses random module to give attendance
+       Parameter:
+        None
+       Return:
+        Prints if the employee is present or absent"""
+
     random_checker=random.randint(0,1)
     if random_checker==1:
         print("The Employee is present")
